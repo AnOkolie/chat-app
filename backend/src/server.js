565e2006de1d9de2bc2f5d4901204fa685d755d3
Anthony <okoliea1@myumanitoba.ca>
2025-11-10 17:48:49 -0600

    Set up deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,24 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import path from "path"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 dotenv.config();
 const app = express()
 
+const __dirname = path.resolve()
 const port  = process.env.PORT
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+console.log(process.env.NODE_ENV)
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*",(_,res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html")) //serve the index.html file for other api routes
+    })
+}
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)

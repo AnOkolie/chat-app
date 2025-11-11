e45b4f4ecb85a5208608753030319a1e97797914
Anthony <okoliea1@myumanitoba.ca>
2025-11-10 19:08:29 -0600

    engines field added in package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,18 @@ import dotenv from "dotenv"
 import path from "path"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+
 dotenv.config();
+
 const app = express()
-
 const __dirname = path.resolve()
-const port  = process.env.PORT
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
-console.log(process.env.NODE_ENV)
+const port  = process.env.PORT || 3000;
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
+
+
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
     app.get("*",(_,res) => {
@@ -20,7 +22,6 @@ if(process.env.NODE_ENV === "production"){
     })
 }
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-
-
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})

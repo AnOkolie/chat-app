2ea3287aa14d684e3ec61a4968fbd037457b8f5d
Anthony <okoliea1@myumanitoba.ca>
2025-11-11 23:15:48 -0600

    rate limiting implementation using arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,12 +2,15 @@ import express from "express"
 import { signup,login, logout } from "../controllers/auth.controllers.js"
 import { protectRoute } from "../middleware/auth.middleware.js"
 import { updateProfile } from "../controllers/auth.controllers.js"
+import { arcjetProtection } from "../lib/arcjet.middleware.js"
 
 const router = express.Router()
+router.use(arcjetProtection) //use protection for all methods
 
+router.get("/test", (req,res) => res.status(200).json({message:"Test route"}))
 router.post("/signup", signup)
 
-router.post("/login",login)
+router.post("/login",arcjetProtection,login)
 
 router.post("/logout", logout)
 

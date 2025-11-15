1cdfb90b22d17ad09e4e4ad9c5156342fc95ad35
Anthony <okoliea1@myumanitoba.ca>
2025-11-15 04:17:46 -0600

    creates chat page layout and related components and functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,16 +6,17 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js"
 import cors from "cors";
+import { ENV } from "./lib/env.js"
 
 dotenv.config();
 
 const app = express()
 const __dirname = path.resolve()
-
+console.log(ENV.CLIENT_URL)
 const port  = process.env.PORT || 3000;
 
-app.use(express.json()) //allows you to get the fields from the user 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json({ limit: "5mb" })); //allows you to get the fields from the user 
+app.use(cors({ origin:ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
